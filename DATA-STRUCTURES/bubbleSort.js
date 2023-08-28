15c4912356f93b342a318d51e369299a5a8d0aea
Abdimatin <abdimatmicrosoft@gmail.com>
2023-08-28 15:15:56 +0300

    Bubble Sort

---
+++ DATA-STRUCTURES/bubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr){
+    let swapped;
+    do {
+        swapped = false;
+        for(let i = 0; i < arr.length - 1; i++){
+            if(arr[i] > arr[i + 1]){
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        } 
+    } while (swapped);
+    
+}
+
+const Arr = [6,4,12,10,9];
+bubbleSort(Arr);
+console.log(Arr);
