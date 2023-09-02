410627626bfeec89acb39eb96332db8c54a342d6
Abdimatin <abdimatmicrosoft@gmail.com>
2023-09-02 23:01:39 +0300

    MergeSort

--- ALGO/mergeSort.js
+++ ALGO/mergeSort.js
@@ -3,7 +3,7 @@ function mergeSort(arr){
         return arr;
     }
 
-    let mid = Math.floor((arr.length - 1) / 2);
+    let mid = Math.floor((arr.length) / 2);
 
     const leftArr = arr.slice(0, mid);
     const rightArr = arr.slice(mid);
@@ -15,16 +15,17 @@ function mergeSort(arr){
 function merge(leftArr, rightArr){
     const sortedArr = []; //Where the all sorted array will be pushed
 
-    while(leftArr && rightArr){
+    while(leftArr && rightArr){ //While the number in the leftarr and rightarr is true, continue the code in while
 
-        if(leftArr[0] <= rightArr[0]){
+        if(leftArr[0] <= rightArr[0]){ //If the the number in left arr is smaller, pushed it to sorted arr and shift the number from right arr
             sortedArr.push(leftArr.shift());
         }
-        else{
+        else{ //if the number in right arr is smaller, pushed to sorted arr and shift it from the right arr
             sortedArr.push(rightArr.shift());
         }
-       return [...sortedArr, ...leftArr, ...rightArr]
+      
     }
+    return [...sortedArr, ...leftArr, ...rightArr]
 }
 
 const arr = [6,4,12,10,9];
